2decdbcdce07ad5f6a2ffbc2826f993b0251da2a
gaojiazhuo <gaojiazhuo@example.com>
2024-11-01 02:12:52 +0800

    源代码

--- README.md
+++
@@ -1,2 +0,0 @@
-# WeChat-Mini-Program-Sales-Platform
-为优秀的文创产品提供一个可投入使用的售卖程序
---
+++ pages/index/index.js
@@ -0,0 +1,35 @@
+PageTransitionEvent({
+  data:{
+
+  },
+  onLoad:function(options){
+
+  },
+  onReady:function(){
+
+  },
+  onShow:function(){
+
+  },
+  onHide:function(){
+
+  },
+  onUnload:function(){
+
+  },
+  onPullDownRefresh:function(){
+
+  },
+  onReachBottom:function(){
+
+  },
+  onShareAppMessage:function(){
+
+  },
+  onPageScroll:function(){
+
+  },
+  onTabItemTap:function(item){
+
+  }
+});
